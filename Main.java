4b9dd9eef0bdb05cf4af3b3a7ab9bb174c8a3bd6
erikplol <terrydityo@gmail.com>
2024-10-22 08:11:12 +0700

    no 1

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Mobil {
+    String merk;
+    String warna;
+    boolean sedangBergerak;
+ 
+    Mobil(String merk, String warna) {
+        this.merk = merk;
+        this.warna = warna;
+        this.sedangBergerak = false;
+    }
+ 
+    void tampilkanDetail() {
+        System.out.println("Merk: " + merk + ", Warna: " + warna);
+    }
+ 
+    void mulaiBergerak() {
+        if (!sedangBergerak) {
+            sedangBergerak = true;
+            System.out.println(merk + " mulai bergerak.");
+        } else {
+            System.out.println(merk + " sudah bergerak.");
+        }
+    }
+}
+ 
+public class Main {
+    public static void main(String[] args) {
+        Mobil mobil1 = new Mobil("Toyota", "Merah"); 
+        mobil1.tampilkanDetail();
+        mobil1.mulaiBergerak();
+    }
+}
